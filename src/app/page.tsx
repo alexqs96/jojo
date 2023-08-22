484c815e91daca9d117a645bd7ea3b9a9e188058
alexqs96 <alexander.mamani.dev@gmail.com>
2023-08-22 18:21:17 -0300

    test api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ import Seasons from "@/components/Seasons"
 import { Suspense } from "react"
 
 export default async function Home() {
+
+  const data = await fetch("https://jojoapi.vercel.app/api/home").then(res => res.json())
+
+  console.log(data?.message);
+  
+
   return (
     <>
     <Suspense fallback={<p>Cargando Animes...</p>}>
